633669e38922d6302a3961b7e8f7c7d82dd01d9f
Akshay Gabrieal R <akshaygabriealr@gmail.com>
2026-07-25 22:57:47 +0530

    Delete app directory

--- app/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Define the 'auth' blueprint. This allows us to modularize authentication routes 
-# and prefix them all (e.g. /auth/login, /auth/signup) in a clean directory structure.
-auth_bp = Blueprint('auth', __name__)
-
-# Import routes at the bottom of the file to prevent circular imports 
-# (since routes will also need to import auth_bp)
-from app.auth import routes
--- app/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-# Initialize the main blueprint which hosts key user dashboards and landing route redirects.
-main_bp = Blueprint('main', __name__)
-
-from app.main import routes
